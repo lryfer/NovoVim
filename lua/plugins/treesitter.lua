8981b3d8b9db99e981de15dc345e67d5192b2b3f
lryfer <xanteraka@proton.me>
2024-10-15 06:02:37 +0200

    add new plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,14 @@ return {
           true
         },
         run = ':TSUpdate',
-        event= "BufRead",
-        config = function ()
-        
-
-        end,
+        event = "BufRead",
+        config = function()
+        require'nvim-treesitter.configs'.setup {
+        ensure_installed = {"c", "cpp", "rust", "lua", "markdown"},
+        auto_install = true,
+        highlight = {
+          enable = true,
+        },
+      }
+    end,
 }
