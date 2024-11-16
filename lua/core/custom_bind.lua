094fbc25d78423dd1b9f2bf49e26ed665521b31d
lryfer <xanteraka@proton.me>
2024-11-16 23:09:48 +0100

    non beccarti un merge conflict

--- lua/core/custom_bind.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader = " "
-vim.diagnostic.config({virtual_text=true})
-vim.cmd[[set expandtab]]
-vim.cmd[[set shiftwidth=2]]
-vim.keymap.set('n', '<leader>tt', vim.cmd.Explore)
-vim.keymap.set('n', '<leader>tl', vim.cmd.Lexplore)
-    
-    
-
--- lua/core/init.lua
+++
@@ -1,2 +0,0 @@
-require("core.custom_bind");
-require("core.lazy");
