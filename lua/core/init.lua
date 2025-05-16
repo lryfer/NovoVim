abae52d50ad5b56d2cfa3423e387058ec330e3de
lryfer <xanteraka@proton.me>
2025-05-16 18:47:36 +0200

    crea file ui

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,3 +1,4 @@
 require("core.custom_bind");
 require("core.lazy");
+require("core.ui");
 require("core.lsp-settings")
--- lua/core/ui.lua
+++ lua/core/ui.lua
@@ -1,33 +1,10 @@
-return{
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-
-  },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
-    },
-    require("noice").setup({
+require("noice").setup({
   lsp = {
     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
     override = {
       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
       ["vim.lsp.util.stylize_markdown"] = true,
       ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-    },
-  },
-  routes = {
-  {
-    filter = {
-      event = "notify",
-      find = "No information available",
-    },
-      opts = {skip = true},
     },
   },
   -- you can enable a preset for easier configuration
@@ -39,4 +16,3 @@ return{
     lsp_doc_border = false, -- add a border to hover docs and signature help
   },
 })
-}
